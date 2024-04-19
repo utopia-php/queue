476a1c81671ee3ce4afa484cdfda2e8555389163
Eldad Fux <eldad.fux@gmail.com>
2024-04-19 13:45:01 +0200

    Fixed workerman tests

--- src/Queue/Server.php
+++ src/Queue/Server.php
@@ -63,65 +63,6 @@ class Server extends Base
     }
 
     /**
-     * If a resource has been created return it, otherwise create it and then return it
-     *
-     * @param string $name
-     * @param bool $fresh
-     * @return mixed
-     * @throws Exception
-     */
-    public function getResource(string $name, bool $fresh = false): mixed
-    {
-        if (!\array_key_exists($name, $this->resources) || $fresh || self::$resourcesCallbacks[$name]['reset']) {
-            if (!\array_key_exists($name, self::$resourcesCallbacks)) {
-                throw new Exception('Failed to find resource: "' . $name . '"');
-            }
-
-            $this->resources[$name] = \call_user_func_array(
-                self::$resourcesCallbacks[$name]['callback'],
-                $this->getResources(self::$resourcesCallbacks[$name]['injections'])
-            );
-        }
-
-        self::$resourcesCallbacks[$name]['reset'] = false;
-
-        return $this->resources[$name];
-    }
-
-    /**
-     * Get Resources By List
-     *
-     * @param array $list
-     * @return array
-     */
-    public function getResources(array $list): array
-    {
-        $resources = [];
-
-        foreach ($list as $name) {
-            $resources[$name] = $this->getResource($name);
-        }
-
-        return $resources;
-    }
-
-    /**
-     * Set a new resource callback
-     *
-     * @param string $name
-     * @param callable $callback
-     * @param array $injections
-     *
-     * @throws Exception
-     *
-     * @return void
-     */
-    public static function setResource(string $name, callable $callback, array $injections = []): void
-    {
-        self::$resourcesCallbacks[$name] = ['callback' => $callback, 'injections' => $injections, 'reset' => true];
-    }
-
-    /**
      * Stops the Queue server.
      * @return self
      */
@@ -306,11 +247,21 @@ class Server extends Base
             });
 
             $this->adapter->start();
-        } catch (Throwable $error) {
-            self::setResource('error', fn () => $error);
-            // foreach ($this->errorHooks as $hook) {
-            //     call_user_func_array($hook->getAction(), $this->getArguments($hook));
-            // }
+        } catch (Throwable $th) {
+            $context = clone $this->container;
+
+            $dependency = new Dependency();
+            $context->set(
+                $dependency
+                    ->setName('error')
+                    ->setCallback(fn () => $th)
+            );
+
+            foreach (self::$shutdown as $hook) { // Global shutdown hooks
+                if (in_array('*', $hook->getGroups())) {
+                    $this->prepare($context, $hook, [], [])->inject($hook, true);
+                }
+            }
         }
         return $this;
     }
--- tests/Queue/servers/Workerman/worker.php
+++ tests/Queue/servers/Workerman/worker.php
@@ -3,29 +3,32 @@
 require_once __DIR__ . '/../../../../vendor/autoload.php';
 require_once __DIR__ . '/../tests.php';
 
+use Utopia\DI\Container;
 use Utopia\Queue;
 use Utopia\Queue\Message;
+use Utopia\Queue\Server;
 
+$container = new Container();
 $connection = new Queue\Connection\Redis('redis');
 $adapter = new Queue\Adapter\Workerman($connection, 12, 'workerman');
-$server = new Queue\Server($adapter);
-$server->job()
+$server = new Server($adapter);
+$server->setContainer($container);
+
+Server::job()
     ->inject('message')
     ->action(function (Message $message) {
         handleRequest($message);
     });
 
-$server
-    ->error()
+Server::error()
     ->inject('error')
     ->action(function ($th) {
         echo $th->getMessage() . PHP_EOL;
     });
 
-$server
-    ->workerStart()
-    ->action(function () {
-        echo "Worker Started" . PHP_EOL;
-    });
+// Server::workerStart()
+//     ->action(function () {
+//         echo "Worker Started" . PHP_EOL;
+//     });
 
 $server->start();
